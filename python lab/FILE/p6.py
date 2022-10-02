310e5e913dcfa311ba032eb5ecc7cf3352dd582f
Divyansh Lappy <divyanshjha30@gmail.com>
2022-10-02 13:21:36 +0530

    Python lab file

--- python lab/FILE/p6.py
+++
@@ -1,6 +0,0 @@
-lst = []
-num = int(input('How many numbers: '))
-for n in range(num):
-    numbers = int(input('Enter number: '))
-    lst.append(numbers)
-print("Largest number in the list is :", max(lst), "\nSmallest number in the list is :", min(lst))
--- python lab/FILE/p8.py
+++
@@ -1,5 +0,0 @@
-lst = ['a','b','c','d','e','f','g','h','i','j']
-print("The list containing first 10 letters of the alphabet: ",lst)
-print("First three letters of the alphabet: ",lst[0:3])
-print("Three letters from the middle are: ",lst[4:7])
-print("All letters from a particular index = (5): ",lst[5:])
