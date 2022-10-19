edd7794a35e868fdd14372208615eed7c0410203
Divyansh Lappy <divyanshjha30@gmail.com>
2022-10-19 10:09:53 +0530

    new update 19/10/22

---
+++ python lab/pythonfppts/a4.py
@@ -0,0 +1,9 @@
+languages=['Hindi', 'English', 'Bengali', 'Oriya', 'Telugu','Marathi', 'Malayalam']
+states=['Delhi', 'Kerala', 'Tamil Nadu', 'Bihar', 'Delhi']
+# print("1.	",languages=languages + "Tamil")
+states=states*2
+print("2.	",states)
+print("3.	",len(languages))
+print("4.	",max(languages))
+print("5.	",min(languages))
+print("6.	",states.count('Delhi'))
---
+++ python lab/pythonfppts/programs/a3.py
@@ -0,0 +1,16 @@
+monuments=[ 'KutubMinar', 10, 'TajMahal', 20, 'India Gate', 30, 'Char Minar', 40]
+states=['Delhi', 'Kerala', 'Tamil Nadu', 'Bihar']
+
+# monuments[5] = 'Red Fort'
+# monuments.append(50)
+# monuments += states
+# monuments[2] = 'India'
+# monuments[3] = 'India'
+# monuments[4] = 'India'
+# monuments.pop(2)
+# for i in range(2,5):
+#     monuments.pop(i)
+
+
+
+print(20 in monuments)
